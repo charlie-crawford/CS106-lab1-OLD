98f9ff9dce061cd1c5290ce55e6fba53df71af59
charlie-crawford <70973330+charlie-crawford@users.noreply.github.com>
2021-06-01 13:54:26 -0400

    Add files via upload

---
+++ cs106-lab1-main/src/main/java/propublica/datadesign/Main.java
@@ -0,0 +1,15 @@
+package propublica.datadesign;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main {
+	// this should become the "Prediction Fails Differently for Black Defendants" table
+	public static PropublicaDataTable racialBiasTable = null;
+	
+    public static void main( String[] args ) {
+        // TODO: eventually set racialBiasTable to a new PropublicaDataTable with correct values.
+    	
+    }
+}
